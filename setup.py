5260ac0d30dfe289610b3aa371246a7ec5bbf7b9
Joseph Benjamin Del Mundo <jbtdelmundo@unionbankph.com>
2023-06-21 13:50:01 +0800

    updated version

--- setup.py
+++ setup.py
@@ -6,13 +6,12 @@ with open("requirements.txt", "r") as fh:
     requirements_txt = fh.read().splitlines()
 
 setup(
-    name='dss_toolkit',
-    version='0.1.0',
-    description='Library for commonly used code for Data Science Experiments',
-    author='Joseph Benjamin Del Mundo',
-    author_email='jbdelmundo@gmail.com',
-    packages=find_packages(include=['dss_toolkit', 'dss_toolkit.*']),
-    
-    install_requires=requirements_txt # Specify required libraries: pkg, pkg ==version or pkg>=version
+    name="dss_toolkit",
+    version="0.1.1",
+    description="Library for commonly used code for Data Science Experiments",
+    author="Joseph Benjamin Del Mundo",
+    author_email="jbdelmundo@gmail.com",
+    packages=find_packages(include=["dss_toolkit", "dss_toolkit.*"]),
+    install_requires=requirements_txt  # Specify required libraries: pkg, pkg ==version or pkg>=version
     # tests
 )
